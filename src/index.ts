3f419af0bfaffd5e68acd6a9078daa4f4a87cc89
ZaneFLS <86607615+ZaneFLS@users.noreply.github.com>
2023-01-23 16:47:42 +0300

    add html

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import io from 'socket.io'
+import path from "path";
 
 const app = express();
 
 const port = 8080;
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '/index.html'));
+})
+
 const start = async() => {
     try {
         app.listen(port, () => {
-            console.log(`Example app listening at http://localhost:${port}`);
+            console.log(`Example app listening at http://localhost:${ port }`);
         });
     } catch (e) {
         console.log(e);
